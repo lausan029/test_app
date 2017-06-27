7e921bc0b3a8bec9f21febd8c947ef78fe592b5e
rails test <lau.san029@gmail.com>
2017-06-27 17:04:39 -0400

    commit con bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-	root 'pages#home'
+  devise_for :users
+  resources :articles
+	root 'pages#index'
 	get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
